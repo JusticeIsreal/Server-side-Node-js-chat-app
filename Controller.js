850361482b31f32c7cde924660c615eb3ef3b7a0
Justice Isreal <justiceyba@gmail.com>
2023-08-10 15:57:13 +0100

    chat engine put request for create or add user endpoint completed

--- Controller.js
+++ Controller.js
@@ -1,6 +1,22 @@
 const authenticateUser = async (req, res) => {
   const { username } = req.body;
-  console.log("yes");
+  try {
+    const resp = axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      {
+        hearders: { private_key: process.env.CHAT_ENGINE_PRIVATE_KEY },
+      }
+    );
+    return res.status(resp.status).json(resp.data);
+  } catch (error) {
+    return res.status(error.resp.status).json(error.resp.data);
+  }
+
   return res.json({ username: username, secret: "1234s" });
 };
 
